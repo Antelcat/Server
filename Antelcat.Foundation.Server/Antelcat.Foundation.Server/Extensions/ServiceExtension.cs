7cba82c4838ed45d1fce2b9aef940cee539676a0
Feast <feast107@gmail.com>
2023-05-24 00:15:49 +0800

    Update ServiceExtension.cs

--- Antelcat.Foundation.Server/Antelcat.Foundation.Server/Extensions/ServiceExtension.cs
+++ Antelcat.Foundation.Server/Antelcat.Foundation.Server/Extensions/ServiceExtension.cs
@@ -89,7 +89,7 @@ namespace Antelcat.Foundation.Server.Extensions
         /// </summary>
         /// <param name="builder"></param>
         /// <returns></returns>
-        public static IHostBuilder UseAutowiredServiceProviderFactory(this IHostBuilder builder) 
+        public static IHostBuilder UseAutowiredServiceProviderFactory(this IHostBuilder builder)
             => builder.UseServiceProviderFactory(new AutowiredServiceProviderFactory<AutowiredAttribute>(
                 ServiceCollectionContainerBuilderExtensions.BuildServiceProvider));
 
@@ -100,32 +100,11 @@ namespace Antelcat.Foundation.Server.Extensions
         /// <param name="builder"></param>
         /// <returns></returns>
         public static IHostBuilder UseAutowiredServiceProviderFactory<TAttribute>(this IHostBuilder builder)
-            where TAttribute : Attribute 
+            where TAttribute : Attribute
             => builder.UseServiceProviderFactory(new AutowiredServiceProviderFactory<TAttribute>(
                 ServiceCollectionContainerBuilderExtensions.BuildServiceProvider));
 
-        /// <summary>
-        /// 使用 <see cref="CachedAutowiredServiceProviderFactory{AutowiredAttribute}"/> 作为服务生成工厂， 
-        /// 实现自动注入携带 <see cref="AutowiredAttribute"/> 注解的属性和字段
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static IHostBuilder UseCachedAutowiredServiceProviderFactory(this IHostBuilder builder) 
-            => builder.UseServiceProviderFactory(new CachedAutowiredServiceProviderFactory<AutowiredAttribute>(
-                ServiceCollectionContainerBuilderExtensions.BuildServiceProvider));
 
-        /// <summary>
-        /// 使用 <see cref="CachedAutowiredServiceProviderFactory{TAttribute}"/> 作为服务生成工厂
-        /// </summary>
-        /// <typeparam name="TAttribute">属性</typeparam>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static IHostBuilder UseCachedAutowiredServiceProviderFactory<TAttribute>(this IHostBuilder builder)
-            where TAttribute : Attribute 
-            => builder.UseServiceProviderFactory(new CachedAutowiredServiceProviderFactory<TAttribute>(
-                ServiceCollectionContainerBuilderExtensions.BuildServiceProvider));
-
-        
         /// <summary>
         /// 将 <see cref="IControllerActivator"/> 的实现替换为 <see cref="AutowiredControllerActivator{AutowiredAttribute}"/> ,
         /// 且应当在 <see cref="MvcCoreMvcBuilderExtensions.AddControllersAsServices"/> 之后调用
@@ -147,27 +126,5 @@ namespace Antelcat.Foundation.Server.Extensions
             where TAttribute : Attribute
             => collection.Services.Replace(ServiceDescriptor
                 .Transient<IControllerActivator, AutowiredControllerActivator<TAttribute>>());
-        
-        /// <summary>
-        /// 将 <see cref="IControllerActivator"/> 的实现替换为 解析 <see cref="AutowiredAttribute"/> 的 <see cref="AutowiredControllerActivator{TAttribute}"/> ,
-        /// 且应当在 <see cref="MvcCoreMvcBuilderExtensions.AddControllersAsServices"/> 之后调用
-        /// </summary>
-        /// <param name="collection"></param>
-        /// <returns></returns>
-        public static IServiceCollection UseCachedAutowiredControllers(this IMvcBuilder collection)
-            => collection.Services.Replace(ServiceDescriptor
-                .Transient<IControllerActivator, CachedAutowiredControllerActivator<AutowiredAttribute>>());
-
-        /// <summary>
-        /// 将 <see cref="IControllerActivator"/> 的实现替换为 解析 <see cref="TAttribute"/> <see cref="AutowiredControllerActivator{TAttribute}"/> ,
-        /// 且应当在 <see cref="MvcCoreMvcBuilderExtensions.AddControllersAsServices"/> 之后调用
-        /// </summary>
-        /// <param name="collection"></param>
-        /// <typeparam name="TAttribute"></typeparam>
-        /// <returns></returns>
-        public static IServiceCollection UseCachedAutowiredControllers<TAttribute>(this IMvcBuilder collection)
-            where TAttribute : Attribute
-            => collection.Services.Replace(ServiceDescriptor
-                .Transient<IControllerActivator, CachedAutowiredControllerActivator<TAttribute>>());
     }
 }
