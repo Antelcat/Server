667a7cdb3b256d80516e149ecf43565c426e6f22
Feast <feast107@gmail.com>
2023-05-23 12:23:34 +0800

    Update AutowiredControllerActivator.cs

--- Antelcat.Foundation.Server/Antelcat.Foundation.Server/Implements/AutowiredControllerActivator.cs
+++ Antelcat.Foundation.Server/Antelcat.Foundation.Server/Implements/AutowiredControllerActivator.cs
@@ -1,30 +1,43 @@
-﻿using Antelcat.Foundation.Core.Attributes;
-using Antelcat.Foundation.Core.Implements.Services;
+﻿using Antelcat.Foundation.Core.Implements.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Antelcat.Foundation.Server.Implements;
 
-public class AutowiredControllerActivator<TAttribute> : IControllerActivator where TAttribute : Attribute
+public abstract class ControllerActivatorBase<TServiceProvider> : IControllerActivator where TServiceProvider : IServiceProvider
 {
-    public object Create(ControllerContext? context)
+    protected abstract TServiceProvider ProvideService(IServiceProvider provider);
+    public object Create(ControllerContext context)
     {
         if (context == null)
             throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
         var type = context.ActionDescriptor.ControllerTypeInfo.AsType();
         var provider = context.HttpContext.RequestServices;
-        if (provider is not AutowiredServiceProvider<TAttribute>)
-            provider = new AutowiredServiceProvider<TAttribute>(context.HttpContext.RequestServices);
+        if (provider is not TServiceProvider)
+            provider = ProvideService(context.HttpContext.RequestServices);
         return provider.GetRequiredService(type);
     }
-
-    public void Release(ControllerContext? context, object? controller)
+    public void Release(ControllerContext context, object controller)
     {
         if (context == null) throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
-        if (controller == null) throw new ArgumentNullException(nameof(controller));
-        if (controller is IDisposable disposable) disposable.Dispose();
+        switch (controller)
+        {
+            case null:
+                throw new ArgumentNullException(nameof(controller));
+            case IDisposable disposable:
+                disposable.Dispose();
+                break;
+        }
     }
 }
 
-public class AutowiredControllerActivator :  AutowiredControllerActivator<AutowiredAttribute>{ }
+public class AutowiredControllerActivator<TAttribute> : ControllerActivatorBase<AutowiredServiceProvider<TAttribute>> where TAttribute : Attribute
+{
+    protected override AutowiredServiceProvider<TAttribute> ProvideService(IServiceProvider provider) => new(provider);
+}
+
+public class CachedAutowiredControllerActivator<TAttribute> : ControllerActivatorBase<CachedAutowiredServiceProvider<TAttribute>> where TAttribute : Attribute
+{
+    protected override CachedAutowiredServiceProvider<TAttribute> ProvideService(IServiceProvider provider) => new(provider);
+}
