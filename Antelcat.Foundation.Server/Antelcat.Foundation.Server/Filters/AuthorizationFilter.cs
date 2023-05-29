1732bee1ae8bd326a1dfeffb2c21a1daec0b773d
Feast <feast107@gmail.com>
2023-05-29 11:03:18 +0800

    Update AuthorizationFilter.cs

--- Antelcat.Foundation.Server/Antelcat.Foundation.Server/Filters/AuthorizationFilter.cs
+++ Antelcat.Foundation.Server/Antelcat.Foundation.Server/Filters/AuthorizationFilter.cs
@@ -4,60 +4,59 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Net;
 
-namespace Antelcat.Foundation.Server.Filters
+namespace Antelcat.Foundation.Server.Filters;
+
+public sealed class AuthorizationFilter : IOperationFilter
 {
-    public sealed class AuthorizationFilter : IOperationFilter
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        operation.Responses.Clear();
+        if (context.ApiDescription.ActionDescriptor is not ControllerActionDescriptor descriptor) return;
+        if (!WhetherActionNeedAuth(descriptor)) return;
+        operation.Parameters.Add(new OpenApiParameter
         {
-            operation.Responses.Clear();
-            if (context.ApiDescription.ActionDescriptor is not ControllerActionDescriptor descriptor) return;
-            if (!WhetherActionNeedAuth(descriptor)) return;
-            operation.Parameters.Add(new OpenApiParameter
+            Name = nameof(Authorization),
+            AllowEmptyValue = true,
+            In = ParameterLocation.Header,
+            Required = true,
+            Description = "This action need auth"
+        });
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
             {
-                Name = nameof(Authorization),
-                AllowEmptyValue = true,
-                In = ParameterLocation.Header,
-                Required = true,
-                Description = "This action need auth"
-            });
-            var securityRequirement = new OpenApiSecurityRequirement
-            {
+                new OpenApiSecurityScheme
                 {
-                    new OpenApiSecurityScheme
+                    Reference = new OpenApiReference
                     {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        },
-                        In = ParameterLocation.Header,
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
                     },
-                    new List<string>()
-                }
-            };
-            operation.Security = new List<OpenApiSecurityRequirement> { securityRequirement };
-        }
-        private static bool WhetherActionNeedAuth(ControllerActionDescriptor descriptor)
-        {
-            var tmp = AnalyzeAuthRequired(descriptor.MethodInfo.GetCustomAttributes(true));
-            return  tmp == -1 
-                ? AnalyzeAuthRequired(descriptor.ControllerTypeInfo.GetCustomAttributes(true)) == 1 
-                : tmp == 1;
-        }
-        private static int AnalyzeAuthRequired(IEnumerable<object> attrs)
-        {
-            foreach (var attr in attrs)
-            {
-                switch (attr)
-                {
-                    case AuthorizeAttribute:
-                        return 1;
-                    case AllowAnonymousAttribute:
-                        return 0;
-                }
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
             }
-            return -1;
+        };
+        operation.Security = new List<OpenApiSecurityRequirement> { securityRequirement };
+    }
+    private static bool WhetherActionNeedAuth(ControllerActionDescriptor descriptor)
+    {
+        var tmp = AnalyzeAuthRequired(descriptor.MethodInfo.GetCustomAttributes(true));
+        return  tmp == -1 
+            ? AnalyzeAuthRequired(descriptor.ControllerTypeInfo.GetCustomAttributes(true)) == 1 
+            : tmp == 1;
+    }
+    private static int AnalyzeAuthRequired(IEnumerable<object> attrs)
+    {
+        foreach (var attr in attrs)
+        {
+            switch (attr)
+            {
+                case AuthorizeAttribute:
+                    return 1;
+                case AllowAnonymousAttribute:
+                    return 0;
+            }
         }
+        return -1;
     }
 }
