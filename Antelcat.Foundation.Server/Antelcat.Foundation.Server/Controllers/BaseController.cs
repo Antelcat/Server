8383aa9a13063b7502eff95d2dcb2af91cddb88d
Feast <feast107@gmail.com>
2023-05-29 11:03:10 +0800

    Move

---
+++ Antelcat.Foundation.Server/Antelcat.Foundation.Server/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Antelcat.Foundation.Core.Attributes;
+using Antelcat.Foundation.Core.Interface.Logging;
+using Antelcat.Foundation.Server.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Antelcat.Foundation.Server.Controllers;
+
+public abstract class BaseController : Controller
+{
+    protected TIdentity Identity<TIdentity>() where TIdentity : class, new()
+        => (TIdentity)(identity ??= new TIdentity().FromClaims(User.Claims));
+    private object? identity;
+
+    [Autowired] protected IAntelcatLogger<BaseController> Logger { get; init; } = null!;
+}
+    
+    
+public abstract class BaseController<TIdentity> : BaseController where TIdentity : class , new()
+{
+    protected TIdentity Identity => identity ??= new TIdentity().FromClaims(User.Claims);
+    private TIdentity? identity;
+}
