8d6e90b3d7af609cc682387e4a40d2dce3a7dee8
Feast <feast107@gmail.com>
2023-05-29 11:03:14 +0800

    Update JwtExtension.cs

--- Antelcat.Foundation.Server/Antelcat.Foundation.Server/Extensions/JwtExtension.cs
+++ Antelcat.Foundation.Server/Antelcat.Foundation.Server/Extensions/JwtExtension.cs
@@ -3,58 +3,77 @@ using Antelcat.Foundation.Core.Interface.Converting;
 using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Antelcat.Foundation.Server.Extensions
+namespace Antelcat.Foundation.Server.Extensions;
+
+public static class JwtExtension<TIdentity> where TIdentity : class
 {
-    public static class JwtExtension<TIdentity>
+    static JwtExtension()
     {
-        private static readonly Type Type = typeof(TIdentity);
-
-        private static readonly IDictionary<PropertyInfo, IValueConverter> ReadableProps = typeof(TIdentity)
-            .GetProperties()
+        var identityType = typeof(TIdentity);
+            
+        var props = identityType.GetProperties();
+        ReadableProps = props
             .Where(static x => x.CanRead)
-            .ToDictionary(static p => p, static p => typeof(string).Converter(p.PropertyType));
+            .ToDictionary(
+                static p => p.Name,
+                static p => new Tuple<Getter<object, object>, IValueConverter>(
+                    p.CreateGetter<object, object>(),
+                    typeof(string).Converter(p.PropertyType)));
 
-        private static readonly IDictionary<PropertyInfo, IValueConverter> WritableProps = typeof(TIdentity)
-            .GetProperties()
-            .Where(static x => x.CanWrite)
-            .ToDictionary(static p => p,static p => typeof(string).Converter(p.PropertyType));
+        WritableProps = props.Where(static x => x.CanWrite)
+            .ToDictionary(
+                static p => p.Name,
+                static p => new Tuple<Setter<object, object>, IValueConverter>(
+                    p.CreateSetter<object, object>(),
+                    typeof(string).Converter(p.PropertyType)));
+    }
 
-        private static TIdentity SetFromClaim(TIdentity instance, Claim claim)
+    private static readonly IDictionary<string, Tuple<Getter<object, object>, IValueConverter>> ReadableProps;
+    private static readonly IDictionary<string, Tuple<Setter<object, object>, IValueConverter>> WritableProps;
+    private static TIdentity SetFromClaim(TIdentity identity, Claim claim)
+    {
+        if (!WritableProps.TryGetValue(claim.Type, out var tuple)) return identity;
+        var i = (object)identity;
+        tuple.Item1.Invoke(ref i!, tuple.Item2.To(claim.Value));
+        return identity;
+    }
+    public static TIdentity? FromToken(TIdentity identity, string token)
+    {
+        try
         {
-            WritableProps
-                .FirstOrDefault(x => x.Key.Name == claim.Type)
-                .Key.SetValue(instance, claim.Value);
-            return instance;
+            return new JwtSecurityToken(token)
+                .Claims
+                .Aggregate(identity, SetFromClaim);
         }
-        public static TIdentity? FromToken(string token)
+        catch
         {
-            try
-            {
-                return new JwtSecurityToken(token)
-                    .Claims
-                    .Aggregate((TIdentity)Type.RawInstance(), SetFromClaim);
-            }
-            catch
-            {
-                return default;
-            }
-        }
-        public static IEnumerable<Claim> GetClaims(TIdentity source)
-        {
-            return ReadableProps
-                .Select(x => 
-                    new Claim(x.Key.Name, (string?)x.Value.Back(x.Key.GetValue(source)) ?? string.Empty));
-        }
-        public static TIdentity FromClaims(IEnumerable<Claim> claims)
-        {
-            return claims.Aggregate((TIdentity)Type.RawInstance(),static (i, c) =>
-            {
-                var pair = WritableProps.FirstOrDefault(x => x.Key.Name == c.Type);
-                if (pair.Key.IsNull()) return i;
-                pair.Key.SetValue(i, pair.Value.To(c.Value));
-                return i;
-            });
+            return default;
         }
     }
+    public static IEnumerable<Claim> GetClaims(TIdentity identity) =>
+        ReadableProps
+            .Select(x =>
+                new Claim(x.Key, 
+                    (string?)x.Value.Item2.Back(x.Value.Item1.Invoke(identity)) ?? string.Empty));
+
+    public static TIdentity FromClaims(TIdentity identity, IEnumerable<Claim> claims) =>
+        claims.Aggregate(identity, SetFromClaim);
+}
+
+public static class JwtExtension
+{
+    public static TIdentity FromClaims<TIdentity>(this TIdentity identity, IEnumerable<Claim> claims)
+        where TIdentity : class =>
+        JwtExtension<TIdentity>.FromClaims(identity, claims);
+
+    public static IEnumerable<Claim> GetClaims<TIdentity>(this TIdentity identity)
+        where TIdentity : class =>
+        JwtExtension<TIdentity>.GetClaims(identity);
+
+    public static TIdentity? FromToken<TIdentity>(this TIdentity identity, string token)
+        where TIdentity : class => 
+        JwtExtension<TIdentity>.FromToken(identity, token);
+  
 }
