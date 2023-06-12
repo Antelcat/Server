94d1f99e5fd08bdaed765e5c5c90c4326c42ecb5
Feast <feast107@gmail.com>
2023-06-12 14:59:25 +0800

    Update

---
+++ src/Antelcat.Server/Hubs/BaseHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Antelcat.Extensions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Antelcat.Server.Hubs;
+
+public abstract class BaseHub : Hub
+{
+    protected TIdentity Identity<TIdentity>() where TIdentity : class, new()
+        => (TIdentity)(identity ??= new TIdentity().FromClaims(Context.User?.Claims ?? new List<Claim>()));
+    private object? identity;
+}
