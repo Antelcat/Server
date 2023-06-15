57fc8beb799c3240b650860406eb332d77bb47e1
Feast <feast107@gmail.com>
2023-06-16 01:17:52 +0800

    Update BaseHub.cs

--- src/Antelcat.Server/Hubs/BaseHub.cs
+++ src/Antelcat.Server/Hubs/BaseHub.cs
@@ -9,7 +9,7 @@ namespace Antelcat.Server.Hubs;
 public abstract class BaseHub : Hub
 {
     protected TIdentity Identity<TIdentity>() where TIdentity : class
-        => (TIdentity)(identity ??= typeof(TIdentity).RawInstance().FromClaims(Context.User?.Claims ?? new List<Claim>()));
+        => (TIdentity)(identity ??= (typeof(TIdentity).RawInstance() as TIdentity)!.FromClaims(Context.User?.Claims ?? new List<Claim>()));
     private object? identity;
 }
 
