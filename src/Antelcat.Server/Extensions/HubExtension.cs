e7ad7962f591e1fe1be6061ceb008111258b47cd
Feast <feast107@gmail.com>
2023-07-24 09:20:17 +0800

    Update

---
+++ src/Antelcat.Server/Extensions/HubExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Antelcat.Server.Extensions;
+
+public static class HubExtension
+{
+    public static Task AddToGroupAsync(this Hub hub, string groupName) => 
+        hub.Groups.AddToGroupAsync(groupName, hub.Context.ConnectionId);
+
+    public static Task RemoveFromGroupAsync(this Hub hub, string groupName) =>
+        hub.Groups.RemoveFromGroupAsync(groupName, hub.Context.ConnectionId);
+}
