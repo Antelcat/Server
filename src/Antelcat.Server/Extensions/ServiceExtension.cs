44308699d722c8c0419b66a975732fce690261a0
Feast <feast107@gmail.com>
2023-11-17 15:36:19 +0800

    Update ServiceExtension.cs

--- src/Antelcat.Server/Extensions/ServiceExtension.cs
+++ src/Antelcat.Server/Extensions/ServiceExtension.cs
@@ -4,22 +4,25 @@ using Antelcat.Server.Filters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Antelcat.Server.Extensions;
 
 public static partial class ServiceExtension
 {
-    public static IServiceCollection AddJwtSwaggerGen(this IServiceCollection collection)
-        => collection.AddSwaggerGen(static o =>
+    public static IServiceCollection AddJwtSwaggerGen(this IServiceCollection collection,
+        Action<SwaggerGenOptions>? config = null)
+        => collection.AddSwaggerGen(o =>
         {
             o.OperationFilter<AuthorizationFilter>();
             o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
                 Description = "Jwt Token Format like [ Bearer {Token} ]",
-                Name = nameof(Authorization),
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey
+                Name        = nameof(Authorization),
+                In          = ParameterLocation.Header,
+                Type        = SecuritySchemeType.ApiKey
             });
+            config?.Invoke(o);
         });
 
     public static IConfiguration GetConfiguration(this IServiceProvider serviceProvider) =>
