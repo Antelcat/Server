98c540768d8160c99cb248e478ff4a085cc417ff
Feast <feast107@gmail.com>
2023-11-26 20:09:45 +0800

    Update

--- src/Antelcat.Server.Test/Controllers/ErrorController.cs
+++ src/Antelcat.Server.Test/Controllers/ErrorController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Net;
+using System.Security.Claims;
 using Antelcat.Server.Controllers;
 using Antelcat.Server.Exceptions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Antelcat.Server.Test.Controllers;
@@ -19,5 +23,27 @@ public class ErrorController : BaseController<ErrorController>
                 message = "Error occurred"
             }
             , "This is an error defined");
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> LoginMultiClaim([FromBody] List<Tuple<string, string>> args)
+    {
+        await Request.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(new ClaimsIdentity(
+                args.Select(x => new Claim(x.Item1, x.Item2)),
+                "Identity.Application")),
+            new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.MaxValue
+            });
+        return Ok();
+    }
+
+    [HttpGet]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public List<(string Key, string Value)> GetClaim()
+    {
+        return HttpContext.User.Claims.Select(x => (x.Type, x.Value)).ToList();
     }
 }
